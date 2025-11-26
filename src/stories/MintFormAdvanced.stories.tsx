b0385d43983dbebac678024af81f5987b1881b23
Kadimi Jaswanth <rishikadimi7@gmail.com>
2025-11-26 19:03:52 +0530

    Add src/stories/MintFormAdvanced.stories.tsx

---
+++ src/stories/MintFormAdvanced.stories.tsx
@@ -0,0 +1,6 @@
+// Story: MintFormAdvanced
+import React from 'react';
+import MintFormAdvanced from '../components/MintFormAdvanced';
+
+export default { title: 'Mint/FormAdvanced' };
+export const Default = () => <MintFormAdvanced />;
