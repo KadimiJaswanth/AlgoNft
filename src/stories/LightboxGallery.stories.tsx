743c572c368ea26ff052e3675777757d2d043de5
Kadimi Jaswanth <rishikadimi7@gmail.com>
2025-11-26 19:03:52 +0530

    Add src/stories/LightboxGallery.stories.tsx

---
+++ src/stories/LightboxGallery.stories.tsx
@@ -0,0 +1,6 @@
+// Story: LightboxGallery
+import React from 'react';
+import LightboxGallery from '../components/LightboxGallery';
+
+export default { title: 'Gallery/Lightbox' };
+export const Default = () => <LightboxGallery />;
