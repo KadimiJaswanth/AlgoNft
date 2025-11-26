78569e52a2e3b2dddba30408d4c1b6066a7bc3a0
Kadimi Jaswanth <rishikadimi7@gmail.com>
2025-11-26 19:03:53 +0530

    Add src/stories/TxRowDetailed.stories.tsx

---
+++ src/stories/TxRowDetailed.stories.tsx
@@ -0,0 +1,6 @@
+// Story: TxRowDetailed
+import React from 'react';
+import TxRowDetailed from '../components/TxRowDetailed';
+
+export default { title: 'Transactions/TxRowDetailed' };
+export const Default = () => <TxRowDetailed id="tx-123" />;
