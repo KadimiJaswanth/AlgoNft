b858b46d9bec6cf73335f50f8347e249ac0147f5
Kadimi Jaswanth <rishikadimi7@gmail.com>
2025-11-26 19:03:53 +0530

    Add src/stories/WalletConnectModal.stories.tsx

---
+++ src/stories/WalletConnectModal.stories.tsx
@@ -0,0 +1,6 @@
+// Story: WalletConnectModal
+import React from 'react';
+import WalletConnectModal from '../components/WalletConnectModal';
+
+export default { title: 'Wallet/ConnectModal' };
+export const Default = () => <WalletConnectModal />;
