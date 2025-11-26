9a932dd4b47f7029373c508b238bbf95cfbcdd8b
Kadimi Jaswanth <rishikadimi7@gmail.com>
2025-11-26 19:03:52 +0530

    Add src/stories/SendPaymentQuick.stories.tsx

---
+++ src/stories/SendPaymentQuick.stories.tsx
@@ -0,0 +1,6 @@
+// Story: SendPaymentQuick
+import React from 'react';
+import SendPaymentQuick from '../components/SendPaymentQuick';
+
+export default { title: 'Payments/SendQuick' };
+export const Default = () => <SendPaymentQuick />;
