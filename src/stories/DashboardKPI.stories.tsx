932abf2ce7ad635dd6c84107c8a3cc38d3939b0e
Kadimi Jaswanth <rishikadimi7@gmail.com>
2025-11-26 19:03:52 +0530

    Add src/stories/DashboardKPI.stories.tsx

---
+++ src/stories/DashboardKPI.stories.tsx
@@ -0,0 +1,6 @@
+// Story: DashboardKPI
+import React from 'react';
+import DashboardKPI from '../components/DashboardKPI';
+
+export default { title: 'Dashboard/KPI' };
+export const Default = () => <DashboardKPI label="Users" value="1.2k" />;
