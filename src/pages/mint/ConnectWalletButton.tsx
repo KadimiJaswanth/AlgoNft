0e34e0e650a16fdff95a9046edf8e477befa3687
Kadimi Jaswanth <rishikadimi7@gmail.com>
2025-11-30 16:20:20 +0530

    Add src/pages/mint/ConnectWalletButton.tsx

---
+++ src/pages/mint/ConnectWalletButton.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ConnectWalletButton: React.FC = () => {
+  return (
+    <button className="px-4 py-2 bg-[#00e5ff] text-black rounded-lg font-semibold shadow-neon">Connect Wallet</button>
+  );
+};
+
+export default ConnectWalletButton;
